33508160dd7d26c05190c7d941c19422f11ad90b
dzakiputraa <pdzaki766@gmail.com>
2026-01-14 08:55:24 +0700

    masukanteks

--- src/pages/Beranda.jsx
+++ src/pages/Beranda.jsx
@@ -1,6 +1,17 @@
 const Beranda = () => {
   return (
-    <div>Beranda</div>
+    <div className="text-white bg-#191919">
+      <h1>X-TJKT 2</h1>
+        <div>
+          <h1>Selamat Datang Di Website</h1>
+          <p>Kelas X-TJKT 2 SMKN 13 Bandung</p>
+        </div>
+
+        <div>
+          Halo Thirteenagers! Kenalin nih kita dari kelas X-TJKT 2 SMKN 13 Bandung Angkatan ke 20 dengan
+          julukan Vimshati Bhakti Amita Paramartha (VBAP) yang berarti angkatan ke 20 yang
+        </div>
+    </div>
   )
 }
 
